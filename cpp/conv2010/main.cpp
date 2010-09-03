fc34a7fc569a28f53366c6457dcd299e424ac226
Johnson Lin <archilifelin@yahoo.com.tw>
2010-09-03 18:20:23 +0800

    initial commit :

---
+++ cpp/conv2010/main.cpp
@@ -0,0 +1,24 @@
+//This is c++0x code.
+#include <cstdio>
+
+namespace conv2010
+{
+class Records;
+class CoC; //code of conduct
+class Manual;
+class Memo;
+
+class CoC
+{
+    //code of conduct
+
+
+}; //class CoC
+
+} //conv2010
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
