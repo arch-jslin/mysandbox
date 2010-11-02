232c0c6ba28ecadc7b9366590fc18bf66a7972ce
Johnson Lin <archilifelin@yahoo.com.tw>
2010-11-02 14:48:00 +0800

    end of table clarification examples.

--- lua/test2.lua
+++ lua/test2.lua
@@ -10,13 +10,33 @@ for k,v in pairs(t) do print(k,v) end
 print("---")
 for k,v in ipairs(t) do print(k,v) end
 print("---")
+
 local u = {}
 for i = -10, 10 do u[i] = i end
 for k,v in pairs(u) do print(k,v) end
 print("---")
 for k,v in ipairs(u) do print(k,v) end
 print("Number of elements in u's array: " .. #u)
+print("---")
 
 local v = {[0]='a'} -- index as 0 is not in the array part of table
 print(v[0]) -- outputs: a
-print(#v) -- outputs: 0 
+v[#v+1] = 'cc'
+print(#v) -- outputs: 1
+v[500] = 2
+print(#v)
+for k,v in pairs(v) do print(k,v) end
+print("---")
+for k,v in ipairs(v) do print(k,v) end
+print("max index of table v: " .. table.maxn(v))
+print("---")
+
+local i = 10; local j = "10"; local k = "+10"
+v[i] = "aaa"
+v[j] = "bbb"
+v[k] = "ccc"
+print( v[i]..v[j]..v[k]..v[tonumber(j)]..v[tonumber(k)] )
+
+-- Basics of functions.. ---------------------------
+
+
