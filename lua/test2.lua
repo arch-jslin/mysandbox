aeb0faf27287779ffc2222f4e8a7d3a978fb5cb7
Johnson Lin <archilifelin@yahoo.com.tw>
2010-11-02 14:34:47 +0800

    add some table clarification examples.

--- lua/test2.lua
+++ lua/test2.lua
@@ -1,6 +1,22 @@
 -- some basic/rudimentary table usages ------------------
 
+local t = {}
+t[1] = 10
+t[0.5] = 15
+t["1"] = 20
+print( #t )
 
+for k,v in pairs(t) do print(k,v) end
+print("---")
+for k,v in ipairs(t) do print(k,v) end
+print("---")
+local u = {}
+for i = -10, 10 do u[i] = i end
+for k,v in pairs(u) do print(k,v) end
+print("---")
+for k,v in ipairs(u) do print(k,v) end
+print("Number of elements in u's array: " .. #u)
 
-
-
+local v = {[0]='a'} -- index as 0 is not in the array part of table
+print(v[0]) -- outputs: a
+print(#v) -- outputs: 0 
