3bae9d867e347110d27a34f1739f7564a7712b5d
Archilife Lin <archilifelin@yahoo.com.tw>
2010-11-19 03:00:07 +0800

    add user side

--- lua/guess1.lua
+++ lua/guess1.lua
@@ -6,32 +6,38 @@ local random = function(n)
   return floor(rand()*abs(n)) 
 end
 
+local ask_for_hint = function(g)
+  io.write("Is your answer "..g.."? (too (b)ig/too (s)mall/(y)es): ")
+	return io.read()
+end
+
+local ask_for_your_guess = function(lb, ub)
+  io.write("Your guess ("..lb.."~"..ub.."): ")
+  return io.read()
+end
+
 local AIPhase = function(lowerb, upperb)
   local guess = lowerb + random(upperb - lowerb)
-  local ask_for_hint = function(g)
-    io.write("Is your answer "..g.."? (too (b)ig/too (s)mall/(y)es): ")
-	return io.read()
-  end
   repeat 
-    local hint = ask_for_hint(guess)
-	while hint ~= "b" and hint ~= "s" and hint ~= "y" do
-	  hint = ask_for_hint(guess)
-	end
-	if hint == "b" then
-	  upperb = guess
-	  guess = floor((upperb + lowerb)/2)
-	  coroutine.yield()
-	elseif hint == "s" then
-	  lowerb = guess
-	  guess = floor((upperb + lowerb)/2)
-	  coroutine.yield()
-	else -- got it
-	  print("Got'cha!")
-	  return true
-	end
-  until upperb - lowerb <= 1
-  print("Then your answer must be "..lowerb.."!")
-  return true -- means "win"
+    local hint = nil
+	  repeat
+	    hint = ask_for_hint(guess)
+	  until hint == "b" or hint == "s" or hint == "y"
+	  if hint == "b" then
+	    upperb = guess - 1
+	    guess = floor((upperb + lowerb)/2)
+	    coroutine.yield(guess)
+	  elseif hint == "s" then
+	    lowerb = guess + 1
+	    guess = floor((upperb + lowerb)/2)
+	    coroutine.yield(guess)
+  	else -- got it
+  	  print("Got'cha!")
+	    return false
+  	end
+  until upperb <= lowerb
+  print("Your answer must be "..lowerb.."!")
+  return false -- means "computer wins"
 end
 
 local main = function(lowerb, upperb)
@@ -40,9 +46,25 @@ local main = function(lowerb, upperb)
     lowerb, upperb = upperb, lowerb
   end
   math.randomseed(os.time()) -- renew seed for every process.
-  ai_phase = coroutine.wrap(function() return AIPhase(lowerb, upperb) end)
+  local ai_phase = coroutine.wrap(function() return AIPhase(lowerb, upperb) end)
+  local ai_ans   = lowerb + random(upperb - lowerb)
   
-  while not ai_phase() do end
+  repeat 
+    local guess, res = nil, true
+    repeat 
+      guess = tonumber(ask_for_your_guess(lowerb, upperb-1))
+    until guess and guess >= lowerb and guess < upperb
+    if guess > ai_ans then
+      print("Your guess is too big.")
+      res = ai_phase()
+    elseif guess < ai_ans then
+      print("Your guess is too small.")
+      res = ai_phase()
+    else -- you got it
+      print("You got it.")
+      break
+    end
+  until not res
 end
 
 main(tonumber(arg[1]), tonumber(arg[2]))
