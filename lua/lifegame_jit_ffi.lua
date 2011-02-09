b878ab696f148e090e6dc09f2623ebfd42a72982
Johnson Lin <archilifelin@yahoo.com.tw>
2011-02-09 18:22:39 +0800

    strange, why all the garbage value after first iteration?

--- lua/lifegame_jit_ffi.lua
+++ lua/lifegame_jit_ffi.lua
@@ -1,4 +1,11 @@
 -- KISS conway's game of life impl ---------------------------
+-- added LuaJIT FFI usages -----------------------------------
+  -- The most annoying change would be zero-based / one-based scalar type differences.
+
+local ffi = require "ffi"
+ffi.cdef[[
+
+]]
 
 local randomseed, rand, floor, abs = math.randomseed, math.random, math.floor, math.abs
 local random = function(n) 
@@ -15,11 +22,11 @@ end
 -------------
 
 local function new_grid(w, h) 
-  local grid = {}
+  local grid = ffi.new("char*[?]", h)
   w, h = w or 15, h or 15
-  for y = 1, h do
-    grid[y] = {}
-    for x = 1, w do
+  for y = 0, h-1 do
+    grid[y] = ffi.new("char[?]", w)
+    for x = 0, w-1 do
       grid[y][x] = 0
     end
   end
@@ -29,43 +36,46 @@ end
 local function grid_print(grid, w, h)
   w, h = w or 15, h or 15
   if not grid then return end
-  for y = 1, h do
-    for x = 1, w do 
+  for y = 0, h-1 do
+    for x = 0, w-1 do 
       io.write(string.format("%d ", grid[y][x]))
     end
     print()
   end
 end
 
-local dir = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}}
+local dir = ffi.new("char[8][2]", {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}})
 local function neighbor_count(old_grid, y, x, h, w)
   local count = 0
-  for i=1, #dir do
-    local ny, nx = y + dir[i][1], x + dir[i][2]
-    if ny < 1 then ny = h
-    elseif ny > h then ny = 1 end
-    if nx < 1 then nx = w
-    elseif nx > w then nx = 1 end
+  for i=0, 7 do
+    local ny, nx = y + dir[i][0], x + dir[i][1]
+    if ny < 0 then ny = h-1
+    elseif ny > h then ny = 0 end
+    if nx < 0 then nx = w-1
+    elseif nx > w then nx = 0 end
     if old_grid[ny][nx] > 0 then count = count + 1 end
   end
+  print("count trace: "..count)
   return count
 end
 
-local rule1 = {0, 0, 1, 1, 0, 0, 0, 0, 0}
-local rule2 = {0, 0, 0, 1, 0, 0, 0, 0, 0}
+local rule1 = ffi.new("char[9]", {0, 0, 1, 1, 0, 0, 0, 0, 0});
+local rule2 = ffi.new("char[9]", {0, 0, 0, 1, 0, 0, 0, 0, 0}); 
 local function ruleset(now, n)
-  return now > 0 and rule1[n+1] or rule2[n+1]
+  return now > 0 and rule1[n] or rule2[n]
 end
 
 local function grid_iteration(old_grid, new_grid, w, h)
   w, h = w or 15, h or 15
-  for y = 1, h do
-    for x = 1, w do
+  for y = 0, h-1 do
+    for x = 0, w-1 do
+      io.write(string.format("%4d", old_grid[y][x]))
       new_grid[y][x] = ruleset( old_grid[y][x], neighbor_count(old_grid, y, x, h, w) )
     end
+    print''
   end
-  for y = 1, h do 
-    for x = 1, w do 
+  for y = 0, h-1 do 
+    for x = 0, w-1 do 
       old_grid[y][x] = new_grid[y][x] -- grid data copy
       new_grid[y][x] = 0              -- clean new grid data
     end
@@ -80,7 +90,7 @@ randomseed(os.time())
 
 local function test_by_hand()
   for i=1, 45 do
-    now[random(15)+1][random(15)+1] = 1  -- random seeding
+    now[random(15)][random(15)] = 1  -- random seeding 45 cells
   end
 
   while true do
@@ -92,12 +102,14 @@ end
 
 local function bench_test(n)
   for i=1, 45 do
-    now[random(15)+1][random(15)+1] = 1  -- random seeding
+    now[random(15)][random(15)] = 1  -- random seeding 45 cells
   end
   local function performance_test(n, now, new)
+    --print("Memory usage before first run: "..collectgarbage("count").." KiB.")
     for i = 1, n do
       grid_iteration(now, new, 15, 15)
     end
+    --print("Memory usage after last run: "..collectgarbage("count").." KiB.")
   end
   grid_print(now, 15, 15)
   bench(string.format("Life's game most easy way %d iter: ", n), 
@@ -105,6 +117,6 @@ local function bench_test(n)
   grid_print(now, 15, 15)
 end
 
-bench_test(20000)
+bench_test(2)
 
 --test_by_hand()
