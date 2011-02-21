b214c6ef824f4f0d2aa219bf1a56226d37ac47fe
Archilife Lin <archilifelin@yahoo.com.tw>
2011-02-22 04:04:17 +0800

    added a 1d VLA test

--- lua/massive_GC.lua
+++ lua/massive_GC.lua
@@ -17,15 +17,21 @@ local function new_2dVLA(w, h)
   return ffi.new("double["..h.."]["..w.."]");
 end
 
+local function new_1dVLA(w, h)
+  return ffi.new("double["..h*w.."]");
+end
+
 --local array2d = new_2d_table(1000, 1000)
 local array2d = new_2dVLA(1000, 1000)
+--local array2d = new_1dVLA(1000, 1000)
 
 local function test(csize)
   local length = 0
   local px = 0
   for y = 1, 999 do
     for x = 1, 999 do
-      array2d[y][x] = x*csize 
+      --array2d[y][x] = x*csize 
+      array2d[length] = x*csize
       length = length + 1
     end
   end
