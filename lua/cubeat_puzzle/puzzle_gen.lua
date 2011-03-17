9829a6fe6749afa8360e50b50d2b489f2902654e
Johnson Lin <archilifelin@yahoo.com.tw>
2011-03-17 16:45:56 +0800

    cleaned the comments up a little bit

--- lua/cubeat_puzzle/puzzle_gen.lua
+++ lua/cubeat_puzzle/puzzle_gen.lua
@@ -8,34 +8,6 @@ local tablex = require 'tablex2'
 local Helper = require 'helpers'
 local random, Stack = Helper.random, Helper.stack
 
--- 實際在檢查上，具現化前還要看 intersects 下去會不會造成 column 高度爆炸，
--- 或是 row 浮空了
-
--- 　function chain_limit
--- 　　//找出能把 30011 截斷的組合 => 故得知查表「A 能被 a b c 截斷」較有效益：
--- 　　stack = {30011} //從能被放到最底下的組合中隨機取一個出來當底
--- 　　iterate on「A 能被 a, b, c, ... 截斷」之 a, b, c；A 為 stack[top]
--- 　　　push one of {a,b,c...} into stack
--- 　　　依照謎題表示法具現盤面，檢查：
--- 　　　不能有人浮空
--- 　　　　(不用具現盤面也能檢查？我可以 keep track 目前盤面每個 row 的範圍，
--- 　　　　 並在用謎題表示法的階段就剔除擺下去一定會浮空者)
--- 　　　不能有 invoke (在還沒上色的情況下會有這問題嗎？)
--- 　　　iterate on colors
--- 　　　　放入發火點，並給該段上色 (必需考慮發火點顏色)，驗證正確性：
--- 　　　　連同發火點考慮，不能有 invoke
--- 　　　　拿掉發火點並測試能否跑到全消盤面，不能剩下東西
--- 　　　　if 都沒剩下 then ++chain and break loop
--- 　　　end
--- 　　　if 所有顏色都試過還是失敗，pop stack
--- 　　　if time >= time_limit then return nil to indicate generation failed
--- 　　　if chain >= chain_limit then break loop
--- 　　end
--- 　　if chain < chain_limit return nil to indicate generation failed
--- 　　將謎題表示式具現化成盤面
--- 　　return 盤面
--- 　end 
-
 math.randomseed(os.time())
 
 local PuzzleGen = {}
@@ -131,15 +103,14 @@ local function color_chain(chains, colors)
   return chains_dup
 end
 
--- 失敗。permutation 方法耗費太多時間在無用 permutation；
--- shuffle 法因為使用 coroutine 結果其實效果不彰。
--- 還可能挑戰的方式：不要 shuffle，只隨機換兩三個，然後不要用 iterator 法 (所以不用 coroutine)
-
--- 其他要馬上加強的點：
--- next_chain 要拆掉，越細越好，確立 operation semantic 而不是一堆 statement
--- 把所有可以先寫死的地方寫死：
---   e.g. 要 if lenV elseif lenH 的地方，要 for i to lenV or lenH 的地方 -- 非常重要
---   有時間再改寫成 meta programming (loadstring) 版本
+-- 'fix' PuzzleGen:
+-- break 'next_chain' apart, into small / modulize methods. don't pile lots of statements 
+-- altogether, rather make sure the operation semantic makes sense literally.
+-- pre-compute everywhere, and remove all the small loops:
+--   1. all the 'if lenV elseif lenH'
+--   2. all the 'for i to lenV or lenH'
+-- => this optimization is very very important for luajit. Always write less branchy code,
+-- => meta programming (loadstring) when I have time or when the need arises.
 
 function PuzzleGen:next_chain()
   local intersects = self.intersects_of[ self.chains:top() ]
