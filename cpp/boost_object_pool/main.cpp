bbb3f1446960afea057641ed71ec659118f639e3
Johnson Lin <arch.jslin@gmail.com>
2011-03-31 23:41:34 +0800

    bug report

--- cpp/boost_object_pool/main.cpp
+++ cpp/boost_object_pool/main.cpp
@@ -1,15 +1,16 @@
 
 #include <iostream>
 #include <sstream>
-//#include <tr1/memory>
+#include <tr1/memory>
 #include <tr1/functional>
 #include <tr1/tuple>
 #include <utility>
 #include <list>
 #include <cstdlib>
 
-#include <boost/shared_ptr.hpp>
-#include <boost/weak_ptr.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/weak_ptr.hpp>
+//#include <boost/tr1/memory.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
@@ -22,64 +23,15 @@
 
 using namespace std;
 using namespace tr1;
-using boost::shared_ptr;
-using boost::weak_ptr;
-
-namespace view {
-    class A{};
-    typedef shared_ptr<A> pA;
-    typedef weak_ptr<A>  wpA;
-}
-
-class B{};
-class C{};
-enum ENUM{D};
-
-typedef function<void(view::pA&, int, int)>         CB;
-typedef tuple<CB const*, B const*, ENUM, view::wpA> Event;
-typedef list<Event>                                 Listener;
-
-void push_tuple(CB const* cb, view::wpA const& pa, B const* b, ENUM const& e)
-{
-    Listener listener;
-    listener.push_back( make_tuple(cb,b,e,pa) );
-}
-
-typedef shared_ptr<char> pchar;
-typedef weak_ptr<char> wpchar;
-using namespace psc;
-using namespace utils;
-
-struct Dummy {
-    Dummy(int i):i_(i){}
-    ~Dummy() {
-        //Logger::i().buf(" Dummy ").buf(this).buf(" is killed.").endl();
-    }
-    int i_;
-};
-typedef shared_ptr<Dummy> pDummy;
 
 struct Data {
     typedef shared_ptr<Data> pointer_type;
-    static pointer_type create(int i, pDummy p) {
-        return ObjectPool<Data>::create(i, p);
-    }
-    static pointer_type create(int i) {
-        return ObjectPool<Data>::create(i);
-    }
-
-
-    Data(int i, pDummy p):d(i), pd(p){}
     Data(int i):d(i){}
     Data():d(0){}
-
-    ~Data(){
-        //Logger::i().buf("Data destructor called.").endl();
-    }
+    ~Data(){}
 
     int d;
     double dd[10];
-    pDummy pd;
 };
 
 typedef Data::pointer_type pData;
@@ -88,24 +40,13 @@ boost::mutex io_mutex;
 
 struct Utils{
     static void clone_a_lot_of_times(pData orig) {
-        volatile Data* big_array[100];
-        //Data bigarray[1000];
-        Logger::i().endl();
-        Logger::i().buf(" ----------- Start of massive allocation ----------- ").endl();
-        for( volatile int i = 0; i < 100; ++i ) {
-            //pData clone = pData(new Data(i, orig->pch));
-            //pData clone = Data::create(i);
-            big_array[i] = new Data(i, pDummy(new Dummy(1)));
-            //bigarray[i].dd[5] = (double)i;
+        Data* big_array[2000];
+        for( volatile int i = 0; i < 2000; ++i ) {
+            big_array[i] = pData(new Data(i));
         }
-        for( volatile int i = 0; i < 100; ++i ) {
+        for( int i = 0; i < 2000; ++i ) {
             delete big_array[i];
-            //Data d(i);
-            //d.dd[3] = 3.0;
         }
-        Logger::i().endl();
-        Logger::i().buf(" ----------- End of massive allocation ----------- ").endl();
-        //delete [] big_array;
     }
 };
 
@@ -113,24 +54,11 @@ class ThreadedClass {
 public:
     typedef boost::mutex::scoped_lock lock;
     void go(vector<pData> const& data, int mult) {
-        //Logger::i().buf("thread ").buf(mult).buf(" before_data_assignment_and_clearance").endl();
         data_ = data;
-        //Logger::i().buf("thread ").buf(mult).buf(" after_data_assignment_and_clearance").endl();
-        //Logger::i().buf("thread ").buf(mult).buf(" says i am alive.").endl();
-
-        //Logger::i().buf(" thread yell ").buf(mult).endl();
         for( int i = 0; i < 1000; ++i ) {
             lock l(mutex_);
-            //Logger::i().buf(" thread ").buf(mult).buf(" before creating new clone.").endl();
-
             data_.push_back(Data::create( i*mult, pDummy(new Dummy(1))));
             Utils::clone_a_lot_of_times(data_.back());
-
-            //Logger::i().buf(" thread ").buf(mult).buf(" after creating new clone.").endl();
-
-            boost::thread::yield();
-            //if( i%30 == 0 )
-                //Logger::i().buf("thread ").buf(mult).buf(" pushing(").buf(i*mult).buf(")").endl();
         }
     }
     pData fetch(unsigned int i) {
@@ -178,20 +106,11 @@ public:
 
     void probe_and_pop(int i) {
         pData a, b, c;
-        //Logger::i().buf("   fetching ...").endl();
         a = tc1.fetch(0); b = tc2.fetch(0); c = tc3.fetch(0);
-        if( i%30 == 0 ) {
-            //Logger::i().buf("main thread fetch from thread1: ").buf(a?a->d:-1).endl()
-            //           .buf("main thread fetch from thread2: ").buf(b?b->d:-1).endl()
-            //           .buf("main thread fetch from thread3: ").buf(c?c->d:-1).endl();
-        }
-        //Logger::i().buf("   popping ...").endl();
         tc1.pop(); tc2.pop(); tc3.pop();
-        //Logger::i().buf("  end of probing and popping").endl();
     }
 
     void join_all() {
-        //Logger::i().buf(" call join all").endl();
         thrd1->join();
         thrd2->join();
         thrd3->join();
@@ -202,12 +121,6 @@ private:
     ThreadedClass tc1, tc2, tc3;
     pDummy pd1, pd2, pd3;
 };
-
-void print_log()
-{
-    for( int i = 0 ; i < 10 ; ++i )
-        Logger::i().buf("HI i am in the thread.").endl();
-}
 
 int main()
 {
--- cpp/boost_object_pool/main2.cpp
+++ cpp/boost_object_pool/main2.cpp
@@ -1,65 +1,28 @@
 
 #include <iostream>
-//#include <tr1/memory> // FUCK YOU TR1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#include <tr1/memory>
 #include <cstdlib>
-
-#include <boost/shared_ptr.hpp>
-#include <boost/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/pool/object_pool.hpp>
-
-#include "Logger.hpp"
-#include "ObjectPool.hpp"
+//#include <boost/tr1/memory.hpp>
 
 using namespace std;
-//using namespace tr1;
-using namespace psc;
-using namespace utils;
 
 struct Dummy {
     Dummy(int i) { i_ = i; }
-    ~Dummy() {
-        //Logger::i().buf(" Dummy ").buf(this).buf(" is killed.").endl();
-        //you can verify this is called. everything "inside of" this object should be released
-        //correctly no matter what.
-        //The only thing is..  when using shared_ptr, there're other things eating up memory.
-    }
+    ~Dummy() {}
     int i_;
 };
-typedef boost::shared_ptr<Dummy> pDummy;
+typedef tr1::shared_ptr<Dummy> pDummy;
 
-void threaded_func1() {
-    for( int i = 0; i < 10000; ++i ) {
-        pDummy dummy = pDummy(new Dummy(1)); //although dummy's life cycle is so short
-        //it won't give back the memory of some mysterious things... it's not new Dummy(1)'s memory.
+void func1() {
+    for( int i = 0; i < 100000; ++i ) {
+        pDummy dummy = pDummy(new Dummy(1));
     }
-//    for( int i = 0; i < 10000; ++i ) {
-//        volatile Dummy* dummy = new Dummy(1);
-//        dummy->i_ = 2;    //anyway make sure it's not optimized by compiler.
-//        delete dummy;     //and you'll find out all is released here correctly. (no mem growth)
-//    }
-//    for( volatile int i = 0; i < 1000000; ++i ) {
-//        volatile Dummy dummy(1);
-//        dummy.i_ = 2;
-//    }
 }
 
 int main()
 {
-    typedef boost::shared_ptr<boost::thread> pThread;
-
-    pThread my_thread_;
-
-    for( int j = 0; j < 1000; ++j ) {
-        my_thread_ = pThread( new boost::thread(&threaded_func1) );
-        my_thread_->join();
-    }
-
-//    for( int j = 0; j < 1000; ++j )
-//        threaded_func1(); //This is the shittiest thing I've encountered lately.
-//        //The memory growth have nothing to do with threads. shared_ptr's done it all.
-
-    Logger::i().buf(" Main Program ends.").endl();
+    for( int j = 0; j < 100000; ++j )
+        func1();
 
     return 0;
 }
