72d088723921592ed3a8106eb71cc47d13f5c2f2
Johnson Lin <arch.jslin@gmail.com>
2011-06-09 02:52:36 +0800

    fix indent

--- lua/orbit/myapp.lua
+++ lua/orbit/myapp.lua
@@ -24,22 +24,22 @@ orbit.htmlify(view, '.+')
 local myapp = orbit.new()
 
 function myapp.index(web)
-   local songlist = {
-      "Sgt. Pepper's Lonely Hearts Club Band",
-      "With a Little Help from My Friends",
-      "Lucy in the Sky with Diamonds",
-      "Getting Better",
-      "Fixing a Hole",
-      "She's Leaving Home",
-      "Being for the Benefit of Mr. Kite!",
-      "Within You Without You",
-      "When I'm Sixty-Four",
-      "Lovely Rita",
-      "Good Morning Good Morning",
-      "Sgt. Pepper's Lonely Hearts Club Band (Reprise)",
-      "A Day in the Life"
-   }
-   return myapp.render_index(songlist)
+  local songlist = {
+    "Sgt. Pepper's Lonely Hearts Club Band",
+    "With a Little Help from My Friends",
+    "Lucy in the Sky with Diamonds",
+    "Getting Better",
+    "Fixing a Hole",
+    "She's Leaving Home",
+    "Being for the Benefit of Mr. Kite!",
+    "Within You Without You",
+    "When I'm Sixty-Four",
+    "Lovely Rita",
+    "Good Morning Good Morning",
+    "Sgt. Pepper's Lonely Hearts Club Band (Reprise)",
+    "A Day in the Life"
+  }
+  return myapp.render_index(songlist)
 end
 
 myapp:dispatch_get(myapp.index, "/")
