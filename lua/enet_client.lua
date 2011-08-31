753d4c11ff190b641a7317df8d877bcd881df007
Johnson Lin <arch.jslin@gmail.com>
2011-08-31 12:54:07 +0800

    client & server connection test 1

--- lua/enet_client.lua
+++ lua/enet_client.lua
@@ -21,13 +21,12 @@ local function main()
   
   local addr  = ffi.new("ENetAddress[1]")
   local event = ffi.new("ENetEvent[1]") 
-  local peer  = ffi.new("ENetPeer*")
 
   enet.enet_address_set_host(addr, "127.0.0.1")
   addr[0].port = 12345;
 
   -- Initiate the connection, allocating the two channels 0 and 1. 
-  peer = enet.enet_host_connect(client, addr, 2, 0);      
+  local peer = enet.enet_host_connect(client, addr, 2, 0);      
   if peer == ffi_helper.NULL then
     print("No available peers for initiating an ENet connection.")
     os.exit(1)
@@ -36,7 +35,8 @@ local function main()
   -- Wait up to 5 seconds for the connection attempt to succeed. 
   if enet.enet_host_service(client, event, 5000) > 0 and
      event[0].type == enet.ENET_EVENT_TYPE_CONNECT then
-    print("Connection to 127.0.0.1:12345 succeeded.")
+    print("Connection succeeded:", event[0].peer.address.host,
+          event[0].peer.address.port)
   else
     -- Either the 5 seconds are up or a disconnect event was
     -- received. Reset the peer in the event the 5 seconds  
--- lua/enet_server.lua
+++ lua/enet_server.lua
@@ -22,22 +22,25 @@ local function main()
   end
   
   local event = ffi.new("ENetEvent[1]")
-  while enet.enet_host_service(serv, event, 5000) > 0 do -- Wait up to 5 sec for an event
-    if event[0].type == enet.ENET_EVENT_TYPE_CONNECT then
-      print("A new client connected from: ", 
-            event[0].peer.address.host,
-            event[0].peer.address.port)
-      event[0].peer.data = "[This is a client]"
-      
-    elseif event[0].type == enet.ENET_EVENT_TYPE_RECEIVE then
-      print(event[0].channelID, event[0].peer.data, event[0].packet.dataLength, 
-            event[0].packet.data)
-      -- use the packet here
-      enet.enet_packet_destroy(event[0].packet) -- done using it, kill it.
-           
-    elseif event[0].type == enet.ENET_EVENT_TYPE_DISCONNECT then
-      print("Disconnected: ", event[0].peer.data)
-      event[0].peer.data = ffi_helper.NULL -- drop it.
+  while true do 
+    while enet.enet_host_service(serv, event, 0) > 0 do -- Wait up to 5 sec for an event
+      if event[0].type == enet.ENET_EVENT_TYPE_CONNECT then
+        print("A new client connected from: ", 
+              event[0].peer.address.host,
+              event[0].peer.address.port)
+        event[0].peer.data = "[This is a client]"
+        
+      elseif event[0].type == enet.ENET_EVENT_TYPE_RECEIVE then
+        print(event[0].channelID, event[0].peer.data, event[0].packet.dataLength, 
+              event[0].packet.data)
+        -- use the packet here
+        enet.enet_packet_destroy(event[0].packet) -- done using it, kill it.
+             
+      elseif event[0].type == enet.ENET_EVENT_TYPE_DISCONNECT then
+        print("Disconnected: ", event[0].peer.data)
+        event[0].peer.data = ffi_helper.NULL -- drop it.
+      end
+      print('hi', event[0].type)
     end
   end
   ffi.C.atexit (enet.enet_deinitialize);
