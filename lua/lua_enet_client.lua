880e6153452c10dcecefe5772ca19686a1ca9159
Johnson Lin <arch.jslin@gmail.com>
2011-09-26 14:59:59 +0800

    updated lua_enet_server example to connect with each other

--- lua/lua_enet_client.lua
+++ lua/lua_enet_client.lua
@@ -4,16 +4,18 @@ local server = host:connect("localhost:6789")
 
 local done = false
 while not done do
-    local event = host:service(100)
-    if event then
-        if event.type == "connect" then
-            print("Connected to", event.peer)
-            event.peer:send("hello world")
-        elseif event.type == "receive" then
-            print("Got message: ", event.data, event.peer)
-            done = true
-        end
+  local event = host:service(100)
+  if event then
+    if event.type == "connect" then
+      print("Connected to", event.peer)
+      event.peer:send("hello world")
+    elseif event.type == "receive" then
+      print("Got message: ", event.data, event.peer)
+      if event.data == "hello world" then
+        done = true
+      end      
     end
+  end
 end
 
 server:disconnect()
--- lua/lua_enet_server.lua
+++ lua/lua_enet_server.lua
@@ -1,9 +1,19 @@
 require "enet"
-local host = enet.host_create"localhost:6789"
+local host = enet.host_create("localhost:"..arg[1])
+local farside
+if arg[2] then
+  farside = host:connect("localhost:"..arg[2])
+end
+
 while true do
-    local event = host:service(100)
-    if event and event.type == "receive" then
-        print("Got message: ", event.data, event.peer)
-        event.peer:send(event.data)
+  local event = host:service(100)
+  if event then
+    if event.type == "receive" then
+      print("Got message: ", event.data, event.peer)
+      event.peer:send(event.data)
+    elseif event.type == "connect" then
+      print("Some one connected.")
+      event.peer:send("Jejeje.")
     end
+  end
 end
