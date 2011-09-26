bf8d9120ff46ce8bcfbaef3600f0d58ca257383c
Johnson Lin <arch.jslin@gmail.com>
2011-09-26 15:27:32 +0800

    ok, now lua_enet_server.lua sends timed message.

--- lua/lua_enet_server.lua
+++ lua/lua_enet_server.lua
@@ -5,15 +5,24 @@ if arg[2] then
   farside = host:connect("localhost:"..arg[2])
 end
 
+local t = 0
+
 while true do
   local event = host:service(100)
   if event then
     if event.type == "receive" then
       print("Got message: ", event.data, event.peer)
-      event.peer:send(event.data)
     elseif event.type == "connect" then
       print("Some one connected.")
-      event.peer:send("Jejeje.")
+      if not farside then 
+        farside = event.peer 
+      end
+      event.peer:send("Greetings.")
     end
   end
+ 
+  if farside and os.clock() - t > 1 then
+    t = os.clock()
+    farside:send "Jejeje."
+  end    
 end
