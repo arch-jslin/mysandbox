a5a952ba3875d3a78b24bb301dbae56021a79eef
Johnson Lin <arch.jslin@gmail.com>
2011-12-30 14:29:45 +0800

    updated some luajit ffi binding tests. it's strangely a little bit faster under more complex situations...?

--- lua/binding_test/simple_jit_ffi.lua
+++ lua/binding_test/simple_jit_ffi.lua
@@ -3,20 +3,21 @@ local ffi = require 'ffi'
 local C = ffi.C
 
 ffi.cdef[[
-typedef struct Someotherclass Someotherclass;
-typedef struct pSimple pSimple;
+typedef struct abc Someotherclass;
+typedef struct cde SimpleBase;
+typedef struct fgh Simple;
 
 Someotherclass* new_Someotherclass();
 char const* Someotherclass_getData(Someotherclass*);
 char const* Someotherclass_setData(Someotherclass*, char const*);
 void Someotherclass__gc(Someotherclass*);
 
-pSimple* new_Simple(int);
-char const* Simple_getName(pSimple*);
-void Simple__gc(pSimple*);
-int Simple_getID(pSimple*);
-void Simple_setID(pSimple*, int);
-void Simple_change_somedata(pSimple*, Someotherclass*);
+Simple* new_Simple(int);
+char const* SimpleBase_getName(SimpleBase*);
+void Simple__gc(Simple*);
+int Simple_getID(Simple*);
+void SimpleBase_setID(SimpleBase*, int);
+void Simple_change_somedata(Simple*, Someotherclass*);
 ]]
 
 -- wrap into class like behavior
@@ -32,12 +33,14 @@ ffi.metatype("Someotherclass", mt2)
 local mt = {}
 mt.__index = mt
 mt.getName = function(self) 
-  return ffi.string(C.Simple_getName(self))
+  return ffi.string(C.SimpleBase_getName(ffi.cast("SimpleBase*", self)))
 end
 mt.getID = C.Simple_getID
-mt.setID = C.Simple_setID
+mt.setID = function(self, n)
+  C.SimpleBase_setID(ffi.cast("SimpleBase*", self), n)
+end
 mt.change_somedata = C.Simple_change_somedata
-ffi.metatype("pSimple", mt)
+ffi.metatype("Simple", mt)
 
 --[[
 local function Simple(...)
@@ -56,22 +59,26 @@ s:change_somedata(d)
 print(s:getName())
 print(s:getID())
 
+local counter = 0
 local t = os.clock()
-for i = 1, 500000000 do 
-  s:setID(12) -- fine
+for i = 1, 50000 do
+  for j = 1, 10000 do  
+    s:setID(s:getID()+1) -- fine
+  end
 end
 print( os.clock() - t )
+print( s:getID() )
 
 t = os.clock()
 for i = 1, 500000000 do
-  C.Simple_setID(s, 12) -- best
+  C.SimpleBase_setID(ffi.cast("SimpleBase*", s), 12) -- best
 end
 print( os.clock() - t )
 
-local test = C.Simple_setID  
+local test = C.SimpleBase_setID  
 t = os.clock()
 for i = 1, 500000000 do 
-  test(s, 18) -- NOT GOOD!
+  test(ffi.cast("SimpleBase*", s), 18) -- NOT GOOD!
 end
 print( os.clock() - t )
 
