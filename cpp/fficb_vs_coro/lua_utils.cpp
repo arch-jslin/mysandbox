0325c0e8f816a49cdfc4ef2ad75be9393ade4d33
Johnson Lin <arch.jslin@gmail.com>
2012-01-08 22:00:44 +0800

    tested various calling method (mainly ffi callback and coroutine using libcoroutine)

---
+++ cpp/fficb_vs_coro/lua_utils.cpp
@@ -0,0 +1,54 @@
+
+#include "lua_utils.hpp"
+
+bool Lua::run_script(lua_State* L, char const* filename) {
+    if( luaL_loadfile(L, filename) || lua_pcall(L, 0, 0, 0) ) {
+        //bad call will return non-zero
+        error(L, "cannot load file: \n  %s", lua_tostring(L, -1) );
+        return true;
+    }
+    return false;
+}
+
+bool Lua::eval(lua_State* L, char const* script) {
+    if( luaL_loadstring(L, script) || lua_pcall(L, 0, LUA_MULTRET, 0) ) {
+        error(L, "eval failed: \n  %s", lua_tostring(L, -1) );
+        return true;
+    }
+    return false;
+}
+
+void Lua::error(lua_State* L, char const* fmt, ...) {
+    va_list argp;
+    va_start(argp, fmt);
+    vfprintf(stderr, fmt, argp);
+    va_end(argp);
+    printf("\n");
+}
+
+void Lua::stack_dump(lua_State* L)
+{
+    int top = lua_gettop(L);
+    for(int i = 1; i <= top; ++i) {
+        int t = lua_type(L, i);
+        printf("%-5d%s\t", i, lua_typename(L, t));
+        switch(t) {
+            case LUA_TSTRING:
+                printf("'%s'", lua_tostring(L, i));
+                break;
+            case LUA_TNUMBER:
+                printf("%g", lua_tonumber(L, i));
+                break;
+            case LUA_TBOOLEAN:
+                printf(lua_toboolean(L, i) ? "true" : "false");
+                break;
+            default:
+                printf("%s", lua_typename(L, t));
+                break;
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
---
+++ cpp/fficb_vs_coro/main.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <boost/tr1/functional.hpp>
+#include "lua_utils.hpp"
+#include "Coro.h"
+
+#include <ctime>
+
+std::tr1::function<void(int)> THE_CALLBACK = 0;
+Coro *mainCoro, *firstCoro;
+
+extern "C" {
+    typedef struct {
+        int type;
+    } Event;
+
+    __declspec(dllexport) void set_callback(void (*cb)(int)) {
+        THE_CALLBACK = cb;
+    }
+
+    __declspec(dllexport) int poll() {
+        Coro_switchTo_(firstCoro, mainCoro);
+        return 100;
+    }
+}
+
+void start_lua(void* context)
+{
+    lua_State* L = static_cast<lua_State*>(context);
+    Lua::call(L, "start_cycle");
+}
+
+void blah(volatile int a)
+{
+    a = a + 1298346712;
+}
+
+int main()
+{
+    mainCoro = Coro_new();
+	Coro_initializeMainCoro(mainCoro);
+	firstCoro = Coro_new();
+
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    Lua::run_script(L, "test.lua");
+
+    std::time_t t = clock();
+    for( int i = 0; i < 10000000; ++i ) {
+        THE_CALLBACK(100);
+    }
+    printf("%ld\n", clock() - t);
+
+    t = clock();
+
+    for( int i = 0; i < 10000000; ++i ) {
+        blah(100);
+    }
+    printf("%ld\n", clock() - t);
+
+    t = clock();
+    for( int i = 0; i < 10000000; ++i ) {
+        Lua::call(L, "func2", 100);
+    }
+    printf("%ld\n", clock() - t);
+
+    t = clock();
+    Coro_startCoro_(mainCoro, firstCoro, (void *)L, start_lua);
+    for( int i = 0; i < 10000000; ++i ) {
+        Coro_switchTo_(mainCoro, firstCoro);
+    }
+    printf("%ld", clock() - t);
+
+    return 0;
+}
