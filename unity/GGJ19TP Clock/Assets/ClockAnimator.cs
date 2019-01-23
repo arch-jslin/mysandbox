5ff4379556aaf6857613a29fc6ccc6825143fc1f
Johnson Lin <arch.jslin@gmail.com>
2019-01-23 14:59:03 +0800

    so the clock is moving now...

--- unity/GGJ19TP Clock/Assets/ClockAnimator.cs
+++ unity/GGJ19TP Clock/Assets/ClockAnimator.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
+using System;
 
 public class ClockAnimator : MonoBehaviour
 {
     public Transform hours, minutes, seconds;
+
+    private const float
+        hoursToDegrees_ = 360f / 12f,
+        minutesToDegrees_ = 360f / 60f,
+        secondsToDegrees_ = 360f / 60f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,10 @@ public class ClockAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DateTime time = DateTime.Now;
+        hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees_);
+        minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees_);
+        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees_);
     }
 }
 
