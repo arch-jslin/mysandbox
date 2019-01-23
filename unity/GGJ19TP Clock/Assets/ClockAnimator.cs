169fe4c1a4a459523c9e6115b4d858ff43b6cc6b
Johnson Lin <arch.jslin@gmail.com>
2019-01-23 15:11:55 +0800

    followed the clock tutorial and got it working.

--- unity/GGJ19TP Clock/Assets/ClockAnimator.cs
+++ unity/GGJ19TP Clock/Assets/ClockAnimator.cs
@@ -4,6 +4,7 @@ using System;
 public class ClockAnimator : MonoBehaviour
 {
     public Transform hours, minutes, seconds;
+    public bool analog;
 
     private const float
         hoursToDegrees_ = 360f / 12f,
@@ -19,10 +20,21 @@ public class ClockAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DateTime time = DateTime.Now;
-        hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees_);
-        minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees_);
-        seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees_);
+        if( analog ) {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            hours.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalHours * -hoursToDegrees_);
+            minutes.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalMinutes * -minutesToDegrees_);
+            seconds.localRotation =
+                Quaternion.Euler(0f, 0f, (float)timespan.TotalSeconds * -secondsToDegrees_);
+        }
+        else {
+            DateTime time = DateTime.Now;
+            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees_);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees_);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees_);
+        }
     }
 }
 
