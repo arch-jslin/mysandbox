418bff90740b4ea1eaca731cf6cd448ce352e6bc
Johnson Lin <arch.jslin@gmail.com>
2019-01-27 09:20:27 +0800

    added button control

---
+++ unity/GGJ19TP Clock/Assets/ButtonsControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloPlay;
+
+public class ButtonsControl : MonoBehaviour
+{
+    GameObject clock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GameObject.Find("Clock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Buttons.GetButtonDown(ButtonType.ONE))
+        {
+            clock.SetActive(!clock.activeSelf);
+        }
+    }
+}
