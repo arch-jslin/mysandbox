73489c900445f875b37b542dc3236c3a9c61c4e5
Johnson Lin <arch.jslin@gmail.com>
2020-01-31 01:26:05 +0800

    love2d lottery long waited update (???)

--- lua/lovelottery3/src/conf.lua
+++ lua/lovelottery3/src/conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.title = "F2GJ 2017 Taipei Lottery v3" 
+    t.title = "FGJ 2019 Taipei Lottery v3" 
     t.window.width = 600 -- t.screen.width in 0.8.0 and earlier
     t.window.height = 600 -- t.screen.height in 0.8.0 and earlier
 end
